295fa2364546cd8b181d8ff5c16054c5cbe449d3
Howardwheeler <howard.wheeler159@gmail.com>
2024-04-28 21:06:06 -0700

    updated html file 4/28

--- resize_question_box.js
+++ resize_question_box.js
@@ -1,5 +1,10 @@
 function resize_question_box(){
     var textarea = document.getElementById("symptoms");
     var question_box = document.getElementById("question_box");
+    var new_question_box_height = textarea.scrollHeight;
+
+    textarea.style.height = "auto";
+    textarea.style.height = (textarea.scrollHeight-20) + "px";
     question_box.style.height = textarea.scrollHeight + "px";
+    max_line_count += 1;
 }
